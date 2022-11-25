9e20d1fe9649567192544bc1bff04145bb955523
OnlineTrainer <mohamedtechdata@gmail.com>
2022-11-25 23:06:59 +0530

    25-11-22

--- HibernateRelationshipProgram/src/str/Customer.java
+++ HibernateRelationshipProgram/src/str/Customer.java
@@ -1,5 +1,28 @@
 package str;
 
-public class Customer {
-
+public class Customer 
+{
+   private int customerId;
+   private String customerName;
+   private int forevenId;
+public int getCustomerId() {
+	return customerId;
+}
+public void setCustomerId(int customerId) {
+	this.customerId = customerId;
+}
+public String getCustomerName() {
+	return customerName;
+}
+public void setCustomerName(String customerName) {
+	this.customerName = customerName;
+}
+public int getForevenId() {
+	return forevenId;
+}
+public void setForevenId(int forevenId) {
+	this.forevenId = forevenId;
+}
+   
+   
 }
