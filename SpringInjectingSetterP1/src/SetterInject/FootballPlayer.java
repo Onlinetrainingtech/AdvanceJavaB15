3b114fd4385f2ada2ee78e4f47f24c51f9403402
OnlineTrainer <mohamedtechdata@gmail.com>
2022-12-01 20:16:49 +0530

    01-12-2022

---
+++ SpringInjectingSetterP1/src/SetterInject/FootballPlayer.java
@@ -0,0 +1,28 @@
+package SetterInject;
+
+public class FootballPlayer implements Player
+{
+	
+	private String football;
+	
+
+	@Override
+	public void play() {
+		
+		System.out.println("I am Playing...");
+		
+	}
+
+
+	public String getFootball() {
+		return football;
+	}
+
+
+	public void setFootball(String football) {
+		this.football = football;
+	}
+	
+	
+   
+}
